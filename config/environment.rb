50f127ef254a4f596c29db67db8883216134cdd1
Ivan Tachev <eragon2333@gmail.com>
2018-03-27 08:57:01 +0000

    Added heroku settings

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,7 @@ ActionMailer::Base.smtp_settings = {
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain         => 'afternoon-wildwood-26630.herokuapp.com',
-    :authentication => :plain.
+    :authentication => :plain,
 }
+
 ActionMailer::Base.delivery_method = :smtp
