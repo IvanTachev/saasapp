279da3abd36a0231d9a6afe0ee594a9d65e645ce
Ivan Tachev <eragon2333@gmail.com>
2018-04-02 09:27:49 +0000

    subtle UI change

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+    before_action :select_plan, only: :new
+    
+    
     #Extend dafault Devise gem behavior
     #so users can sign up with pro plan
     #with special function.
@@ -14,4 +17,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
             end
         end
     end
+    private
+        def select_plan
+            unless (params[:plan] == '1' || params[:plan]=='2')
+            flash[:notice] = "Please select membership plan to sign up"
+            redirect_to root_url
+            end
+        end
 end
