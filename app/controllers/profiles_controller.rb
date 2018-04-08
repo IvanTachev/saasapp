7de966f9cba65145bc7fd0bf3702cff83e177b9c
Ivan Tachev <eragon2333@gmail.com>
2018-04-08 06:07:13 +0000

    Added edit action for useer

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -20,6 +20,24 @@ class ProfilesController < ApplicationController
         end
     end
     
+    #GET request for /users/:user_id/profile/edit
+    def edit
+        @user = User.find(params[id: :user_id])
+        @profile = @user.profile
+    end
+    
+    #PATCH to /users/:user_id/profile
+    def update
+        @user = User.find(params[:user_id])
+        @profile = @user.profile
+        if @profile.update_attributes(profile_params)
+           flash[:success] = "Profile updated"
+           redirect_to user_path(id: params[:user_id])
+        else
+            redner action :edit
+        end
+    end
+    
     private
         def profile_params
             params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
